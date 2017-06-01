6650273028ec7ae2bbbbe593f9c70b02eca63131
Pravdina Elena <helenj@inventos.ru>
2017-06-01 11:32:42 +0300

    add localization files (ru,en)

---
+++ lang/en.js
@@ -0,0 +1,3 @@
+videojs.addLanguage('en', {
+	"The video is temporarily unavailable, please try again later.": "The video is temporarily unavailable, please try again later."
+});
---
+++ lang/ru.js
@@ -0,0 +1,3 @@
+videojs.addLanguage('ru', {
+	"The video is temporarily unavailable, please try again later.": "Видео временно недоступно, повторите попытку позже."
+});
